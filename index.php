bb081afc19383d852c30c17456abc5c2c7b9e01f
Luvumbu <luvumbu.n@gmail.com>
2020-08-13 21:50:37 +0200

    final burger

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head id="head">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>{{title}}</title>
 </head>
+
 <body>
 
-<?php 
+	<?php
 	include("app.php");
 	include("link.php");
-?>
+	?>
+	<style>
+
+	</style>
 </body>
+
 </html>
