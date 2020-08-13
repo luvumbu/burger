6f3f4ced8337f2f49d4dffeb9e6faddf7ec8cffe
Luvumbu <luvumbu.n@gmail.com>
2020-08-13 20:39:41 +0200

    mon premier commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head id="head">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>{{title}}</title>
+</head>
+<body>
+
+<?php 
+	include("app.php");
+	include("link.php");
+?>
+</body>
+</html>
---
+++ vue.js
@@ -0,0 +1,30 @@
+var head = new Vue({
+  el: '#head',
+  data: {    
+    title:"Bokonzi"
+  }
+})
+
+var header = new Vue({
+  el: '#header',
+  data: {
+    
+  }
+})
+
+var section = new Vue({
+  el: '#section',
+  data: {
+    
+  }
+})
+
+var footer = new Vue({
+  el: '#footer',
+  data: {
+    
+  }
+})
+ 
+
+
